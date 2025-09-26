540ab21c6964f3bcf43c88d165dcdd78f26d7293
Jhomayra21 <jhomayramamanichoqueticlla21@gmail.com>
2025-09-25 22:36:44 -0400

    actualizado

--- PrimerParcial/Program.cs
+++ PrimerParcial/Program.cs
@@ -16,9 +16,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
